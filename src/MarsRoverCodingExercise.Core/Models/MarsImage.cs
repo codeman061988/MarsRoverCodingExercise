85bbad20a5a09ace5a5a6ac6f965d9c708e27a59
codeman061988 <codyhicks57@hotmail.com>
2019-12-13 16:31:26 -0600

    Created initial DTO models for application APIs

---
+++ src/MarsRoverCodingExercise.Core/Models/MarsImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverCodingExercise.Core.Models
+{
+    /// <summary>
+    /// DTO which represets a Mars Image
+    /// </summary>
+    public class MarsImage
+    {
+        /// <summary>
+        /// The date which the image was captured
+        /// </summary>
+        public string Date { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The local (app) URL, which represents where the image is stored locally
+        /// </summary>
+        public Uri LocalUrl { get; set; } = null!;
+    }
+}
---
+++ src/MarsRoverCodingExercise.Core/Models/MarsImagesApiResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverCodingExercise.Core.Models
+{
+    /// <summary>
+    /// DTO which represents a response object from the MarsImages API
+    /// </summary>
+    public class MarsImagesApiResponse
+    {
+        /// <summary>
+        /// Represets a collection of mars images
+        /// </summary>
+        public List<MarsImage> MarsImages { get; } = null!;
+    }
+}
