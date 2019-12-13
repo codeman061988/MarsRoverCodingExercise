4d97e056987baca1116ab257fb843dd942fe20dd
codeman061988 <codyhicks57@hotmail.com>
2019-12-13 16:32:12 -0600

    Created initial controller classes for handling API requests

---
+++ src/MarsRoverCodingExercise.Web/Controllers/v1/ControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarsRoverCodingExercise.Web.Controllers.v1
+{
+    /// <summary>
+    /// Provides shared functionality for all V1 controllers
+    /// </summary>
+    public class ControllerBase : Controller
+    {
+
+    }
+}
---
+++ src/MarsRoverCodingExercise.Web/Controllers/v1/MarsPhotoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarsRoverCodingExercise.Web.Controllers.v1
+{
+    /// <summary>
+    /// Represents a RESTful service for Mars Photos
+    /// </summary>
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    [Route("api/v{api-version:apiVersion}/mars-photos")]
+    public class MarsPhotoController : ControllerBase
+    {
+
+    }
+}
