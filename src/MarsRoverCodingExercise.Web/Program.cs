8ea494f4016fb6fc86edb8db533343cdeec1e552
codeman061988 <codyhicks57@hotmail.com>
2019-12-13 21:04:25 -0600

    Added XML comments for FxCop

--- src/MarsRoverCodingExercise.Web/Program.cs
+++ src/MarsRoverCodingExercise.Web/Program.cs
@@ -9,13 +9,25 @@ using Microsoft.Extensions.Logging;
 
 namespace MarsRoverCodingExercise.Web
 {
-    public class Program
+    /// <summary>
+    /// Main Program entry point
+    /// </summary>
+    public static class Program
     {
+        /// <summary>
+        /// Main program method
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Create host builder
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
